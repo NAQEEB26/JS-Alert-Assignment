c288903a2ad56aebc2d7057f7cc375afd6b8f386
NAQEEB UL REHMAN <130925302+NAQEEB26@users.noreply.github.com>
2023-12-03 14:01:12 +0500

    Add files via upload

---
+++ Assigments no 1/app.js
@@ -0,0 +1,16 @@
+// Javascript assignmet number 1
+// Question number 1
+alert("Welcome to my website");
+
+// Question number 2
+alert("Error! Please enter a valid Password.");
+
+// Question number 3
+alert("Welcome to Js land \n Happy coding!");
+
+// Question number 4
+alert("Welcome to JS Land");
+alert("Happy Coding! \n Prevent to this page from creating aditional dailoges");
+
+// Question number 5
+alert("Hello... I can runJS through my web browse's console");
